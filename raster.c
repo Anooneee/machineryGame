587d8767ffeee09b2e36431deb7a8a530f9a18b5
Roflstomp123 <49823160+Roflstomp123@users.noreply.github.com>
2026-02-16 11:35:58 -0700

    adding in for file transfer

--- raster.c
+++ raster.c
@@ -1,2 +1,17 @@
 #include "raster.h"
 
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 400
+
+
+
+
+void plot_pixel(char *base, int x, int y)
+{
+if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT)
+*(base + y * 80 + (x >> 3)) |= 1 << 7 - (x & 7);
+/* ------ -----
+x / 8 x % 8
+*/
+}
