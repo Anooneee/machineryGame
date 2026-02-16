94c04604b90b37af17ee9e040bd728cd057c930f
Roflstomp123 <49823160+Roflstomp123@users.noreply.github.com>
2026-02-16 11:39:04 -0700

    real bitmap system

--- PLOT_CH.C
+++ PLOT_CH.C
@@ -2,13 +2,15 @@
 #include <osbind.h>
 #include <stdio.h>
 
+
 void blank_screen();
 
-int main()
-{
+int main(){
 	char *base = Physbase();
 	char *font;
 	char ch;
+	int i;
+	char byte;
 
 	blank_screen();
 	linea0();                    /* initialize line-a variables */
@@ -16,7 +18,14 @@ int main()
 	ch = Cnecin();
 
 	/* TO DO: plot ch to upper left of screen directly */
-
+	font += ch;
+	
+	for(i = 0; i < 16; i++){ 
+		byte += (i * 256);
+		*(base + i * 80) |= *(font + i * 256);
+	
+	}
+	
 	Cnecin();
 	return 0;
 }
