402c7ec3dbe107c6f4253d5302a608056997ec71
Zach <zachery.brubacher@gmail.com>
2026-04-11 15:25:14 -0600

    VBL_ISR handles music updating now.

--- ISR.C
+++ ISR.C
@@ -22,22 +22,57 @@ Vector install_vector(int num, Vector vector) {
 }
 
 void disable_midi() {
-	long old_ssp = Super(0);
 	*((volatile unsigned char*)0xFFFFFA09) &= ~(1 << 7);
-	Super(old_ssp);
 }
 
 void enable_midi() {
-	long old_ssp = Super(0);
 	*((volatile unsigned char*)0xFFFFFA09) |= (1 << 7);
-	Super(old_ssp);
-}void install_vectors(){
-    Org_VBL = install_vector(TRAP_28, do_vbl);
-    Org_IKBD = install_vector(TRAP_KB, do_kbd);
+}
+
+void install_vectors(){
+    long oldssp;
+    long old_ssp = Super(0);
+    Org_VBL = install_vector(TRAP_28, vbl_isr);
+    Org_IKBD = install_vector(TRAP_KB, kbd_isr);
+    disable_midi();
+    Super(old_ssp);
+}
+
+void uninstall_vectors(){
+    long oldssp;
+    long old_ssp = Super(0);
+    install_vector(TRAP_28, Org_VBL);
+    install_vector(TRAP_KB, Org_IKBD);
+    enable_midi();
+    Super(old_ssp);
 }
 
 void do_vbl(){
+    static int note_time = 0;
+    static int clock_tick = 0;
+    ticks++;
+    clock_tick++;
 
+    /*time music*/
+    if (note_time =< 0) {
+		*current_note = *current_note + 1;
+
+		if (*current_note > 28) {
+			*current_note = 0;
+		}
+        update_music(*current_note);
+		note_time = melody[*current_note][2];
+	}
+	else {
+		note_time--;
+	}
+
+    /*sync events*/ 
+            
+    /*update game model*/
+
+	/*frame buffer stuff*/
+    render_req = 1;
 }
 
 void do_kbd(){
--- ISR.h
+++ ISR.h
@@ -6,6 +6,7 @@ void trap28();
 Vector install_vector(int num, Vector vector);
 
 void install_vectors();
+void uninstall_vectors();
 
 void disable_midi();
 void enable_midi();
