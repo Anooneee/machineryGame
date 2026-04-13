7ba67c6bd7a09839ef50d9be6c1dcdbd6ea9a629
Anooneee <72642342+Anooneee@users.noreply.github.com>
2026-04-12 22:21:29 -0600

    getting 2 bombs

--- set_vid.s
+++ set_vid.s
@@ -1,11 +1,23 @@
-            xdef    _set_vid_base
+            xdef    _set_video_base
 
-_set_vid_base:
-            move.l      4(sp),d0        ; load parameter UINT32 *
-            lsr.l       #8,d0           ; shift right to move high and low bytes to low word
+_set_video_base:
+            move.l      4(sp),d1        ; load parameter UINT32 *
+            lsr.l       #8,d1           ; shift right to move high and low bytes to low word
+
+                                        ; Enter super to access volitile memory
+            clr.l   -(sp)               ; enter supervisor mode with user stack
+            move.w  #$20,-(sp)
+            trap    #1
+            addq.l  #6,sp
+            move.l  d0,d0
+            
             lea         $FF8201,a1      ; load video base pointer
-            movep.w     d0,0(a1)        ; move high and low to new in one go
+            movep.w     d1,0(a1)        ; move high and low to new in one go
+
+                                        ; Exit super
+            move.l  d0,-(sp)   ; return to user mode, restoring
+            move.w  #$20,-(sp)          ;   system stack pointer
+            trap    #1
+            addq.l  #6,sp
 
             rts             ; Return from subroutine
-
-
